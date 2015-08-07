139a2fa9e9e7f76a80ef2f1d30932038c286ba59
David Yaron <yaron@cmu.edu>
2015-08-07 17:35:21 -0400

    more types of fits

--- analrandom.py
+++ analrandom.py
@@ -96,7 +96,8 @@ def write_statistics(name, outf, pars, fit_result):
     outf.write(name)
     outf.write(fit_report(pars))
     outf.write('\n')
-    outf.write('Norm residuals is ' + repr(LA.norm(fit_result.residual)))
+    outf.write('Norm residuals/root(N) is ' \
+       + repr(LA.norm(fit_result.residual)/math.sqrt(len(fit_result.residual))))
     outf.write('\n')
     outf.write('Mean absolute error ' +
                repr(np.mean(np.abs(fit_result.residual))))
@@ -129,10 +130,11 @@ def plot_something(model, pars, exc, title="", filename="", interval=1):
     plt.savefig(filename)
 
 
-def cross_validate(model, fitData, pars, folds=5):
+def cross_validate(model, fitData, pars, outf, folds=5):
     residual1 = partial(residual, model)
     train_errors = []
     test_errors = []
+    test_rms = []
     for i, (train_index, test_index) in enumerate(KFold(len(fitData),
                                                         n_folds=folds,
                                                         shuffle=True)):
@@ -141,10 +143,20 @@ def cross_validate(model, fitData, pars, folds=5):
 
         fit_result = minimize(residual1, pars, args=([], trainData, []))
         train_errors.append(np.abs(fit_result.residual).mean())
+        
 
         errors = [model(d, pars) - d["exc"] for d in testData]
         test_errors.append(np.abs(errors).mean())
-    print "Train:", np.mean(train_errors), "Test:", np.mean(test_errors)
+        test_rms.append(  LA.norm(errors)/math.sqrt(len(errors)) )
+    outf.write('Train '+ repr(np.mean(train_errors)) + ' +- ' + 
+          repr(np.std(train_errors)) + 
+          ' Test  '+ repr(np.mean(test_errors)) + ' +- ' + 
+          repr(np.std(test_errors))+ '\n')
+    outf.write('Test RMS  '+ repr(np.mean(test_rms)) + ' +- ' + 
+          repr(np.std(test_rms))+ '\n')
+    print "Train:", np.mean(train_errors),"+-",np.std(train_errors), \
+        "Test:", np.mean(test_errors),"+-",np.std(test_errors)
+    print "TestRMS:", np.mean(test_rms),"+-",np.std(test_rms)
     fit_result = minimize(residual1, pars, args=([], fitData, []))
     return fit_result
 
@@ -159,7 +171,7 @@ def cos_fits(fitData, exc, outf, end_diff=False):
         pars.add('delta',    value=0.0, vary=end_diff)
         pars.add('npow',     value=cos_pow, vary=False)
 
-        fit_result = cross_validate(model1, fitData, pars)
+        fit_result = cross_validate(model1, fitData, pars, outf)
 
         if not end_diff:
             parvals = pars.valuesdict()
@@ -170,20 +182,27 @@ def cos_fits(fitData, exc, outf, end_diff=False):
         write_statistics(name, outf, pars, fit_result)
 
         title = 'Coupling = cosine to power %d ' % cos_pow
-        plot_something(model1, pars, exc, title=title, filename='thio_rg_cos%d%r.png' % (cos_pow, end_diff))
+        plot_something(model1, pars, exc, title=title, filename='thio_rg_cos%d%r.eps' % (cos_pow, end_diff))
     return beta_res
 
 
-def model3_fits(fitData, exc, outf, end_diff=False):
+def model3_fits(fitData, exc, outf, c_included, beta_included, beta2_included, end_diff=False):
     # Set up paramters
+    bval = 0
+    b2val = 0
+    if beta_included:
+        bval = -1.0
+    if beta2_included:
+        b2val = -1.0
+        
     pars = Parameters()
-    pars.add('alpha',    value=5.0, vary=True)
-    pars.add('const',    value=0.0, vary=True)
-    pars.add('beta',     value=0.0, vary=False)
-    pars.add('beta2',    value=-1.0, vary=True)
-    pars.add('delta',    value=0.0, vary=end_diff)
+    pars.add('alpha',    value=5.0,   vary=True)
+    pars.add('const',    value=0.0,   vary=c_included)
+    pars.add('beta',     value=bval,  vary=beta_included)
+    pars.add('beta2',    value=b2val, vary=beta2_included)
+    pars.add('delta',    value=0.0,   vary=end_diff)
 
-    fit_result = cross_validate(model3, fitData, pars)
+    fit_result = cross_validate(model3, fitData, pars, outf)
 
     parvals = pars.valuesdict()
     beta_mod3 = (parvals['const'], parvals['beta'], parvals['beta2'])
@@ -192,9 +211,15 @@ def model3_fits(fitData, exc, outf, end_diff=False):
     write_statistics(name, outf, pars, fit_result)
 
     title = "Coupling = const + beta2 cos^2(theta) \n"
-    plot_something(model3, pars, exc, title=title, filename='thio_mod3%r.png' % (end_diff))
+    plot_something(model3, pars, exc, title=title, filename='thio_mod3%r_%r.eps' % (beta_included,end_diff))
     return beta_mod3
 
+
+def cos_form(beta_mod3, x_theta):
+    beta = beta_mod3[0] \
+    + beta_mod3[1] * np.abs(np.cos(np.deg2rad(x_theta))) \
+    + beta_mod3[2] * np.power(np.cos(np.deg2rad(x_theta)), 2)
+    return beta
 
 def discrete_fits(fitData, exc, outf, end_diff=False):
     # Set up paramters
@@ -205,13 +230,13 @@ def discrete_fits(fitData, exc, outf, end_diff=False):
                  np.abs(math.cos(i * 3.14 / 180.0)), vary=True)
     pars.add('delta',    value=0.0, vary=end_diff)
 
-    fit_result = cross_validate(model2, fitData, pars)
+    fit_result = cross_validate(model2, fitData, pars, outf)
 
     name = 'Thiophene discrete: delta varied=%r\n' % end_diff
     write_statistics(name, outf, pars, fit_result)
 
     title = "Treating beta at each angle as a separate parameter \n"
-    plot_something(model2, pars, exc, title=title, filename='thio_discrete%r.png' % (end_diff), interval=10)
+    plot_something(model2, pars, exc, title=title, filename='thio_discrete%r.eps' % (end_diff), interval=10)
     return pars
 
 
@@ -222,58 +247,68 @@ if __name__ == "__main__":
     e3  = exc[:,2]
     e4  = exc[:,3]
     """
+    dataChoice = [True, True, True]
     exc = np.loadtxt(open('thio_uniform.csv', 'r'), delimiter=",")
     ran3 = np.loadtxt(open('thio_random_3.csv', 'r'), delimiter=",", skiprows=1)
     ran4 = np.loadtxt(open('thio_random_4.csv', 'r'), delimiter=",", skiprows=1)
 
     # angles is list of angles for the oligomer
     fitData = []
-    for angLength in xrange(1, 4):
-        for angIndex in xrange(exc[:, 0].size):
-            if (np.abs(np.cos(np.deg2rad(exc[angIndex, 0]))) > 0.05):
-                fitData.append(
-                    {'angles': np.full(angLength, exc[angIndex, 0]),
-                     'exc': exc[angIndex, angLength]}
-                )
-    for row in ran3:
-        fitData.append(
-            {'angles': row[:2],
-             'exc': row[2]}
-        )
-    for row in ran4:
-        fitData.append(
-            {'angles': row[:3],
-             'exc': row[3]}
-        )
+    if dataChoice[0]:
+        for angLength in xrange(1, 4):
+            for angIndex in xrange(exc[:, 0].size):
+                if (np.abs(np.cos(np.deg2rad(exc[angIndex, 0]))) > 0.05):
+                    fitData.append(
+                        {'angles': np.full(angLength, exc[angIndex, 0]),
+                         'exc': exc[angIndex, angLength]}
+                    )
+    if dataChoice[1]:
+        for row in ran3:
+            fitData.append(
+                {'angles': row[:2],
+                 'exc': row[2]}
+            )
+    if dataChoice[2]:
+        for row in ran4:
+            fitData.append(
+                {'angles': row[:3],
+                 'exc': row[3]}
+            )
 
     do_cos_fits = True
     do_discrete_fits = True
     do_model3 = True
     with open('randomFits.txt', 'w') as outf:
-        if do_cos_fits:
-            beta_res = cos_fits(fitData, exc, outf)
-        if do_model3:
-            beta_mod3 = model3_fits(fitData, exc, outf)
-        if do_discrete_fits:
-            pars = discrete_fits(fitData, exc, outf)
+        for delta_included in [True, False]:
+            if do_cos_fits:
+                beta_res = cos_fits(fitData, exc, outf ,delta_included)
+            if do_model3:
+                # after outf is c, beta, beta1, delta
+                cos1      = model3_fits(fitData, exc, outf, False, True,  False, delta_included)
+                cos2      = model3_fits(fitData, exc, outf, False, False, True, delta_included)
+                cos3      = model3_fits(fitData, exc, outf, False, True,  True, delta_included)
+                
+                cos1c     = model3_fits(fitData, exc, outf, True,  True,  False, delta_included)
+                cos2c     = model3_fits(fitData, exc, outf, True,  False, True, delta_included)
+                cos3c     = model3_fits(fitData, exc, outf, True,  True,  True, delta_included)
 
-        parvals = pars.valuesdict()
-        x_theta = []
-        y_beta = []
-        y_cos = []
-        y_cos2 = []
-        for i in xrange(0, 181, 10):
-            x_theta.append(i)
-            y_beta.append(parvals['beta%i' % i])
-        y_cos = beta_res[0] * np.abs(np.cos(np.deg2rad(x_theta)))
-        y_cos2 = beta_res[0] * np.power(np.cos(np.deg2rad(x_theta)), 2)
-        y_mod3 = beta_mod3[0] \
-            + beta_mod3[1] * np.abs(np.cos(np.deg2rad(x_theta))) \
-            + beta_mod3[2] * np.power(np.cos(np.deg2rad(x_theta)), 2)
-        plt.figure(2)
-        plt.title('Comparision of beta(theta) across models\n')
-        plt.plot(x_theta, y_beta, 'ro')
-        plt.plot(x_theta, y_cos, 'b-')
-        plt.plot(x_theta, y_cos2, 'g-')
-        plt.plot(x_theta, y_mod3, 'k-')
-        plt.show()
+            if do_discrete_fits:
+                pars = discrete_fits(fitData, exc, outf, delta_included)
+#%%
+parvals = pars.valuesdict()
+x_theta = []
+y_beta = []
+y_cos = []
+y_cos2 = []
+for i in xrange(0, 181, 10):
+    x_theta.append(i)
+    y_beta.append(parvals['beta%i' % i]) 
+plt.figure(2)
+plt.clf()
+plt.title('Comparision of beta(theta) across models\n')
+plt.plot(x_theta, y_beta, 'ro')
+plt.plot(x_theta, cos_form(cos1,x_theta), 'b-')
+plt.plot(x_theta, cos_form(cos2,x_theta), 'g-')
+plt.plot(x_theta, cos_form(cos3c,x_theta), 'k-')
+plt.show()
+plt.savefig('cosforms.eps')
